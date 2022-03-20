20c4f02d7c810eaae5d3fa8d64a9d8aa7478ccc5
Prince Dhingra <princedhingra25@gmail.com>
2022-03-20 13:27:59 -0400

    Update fastExponent.cpp

--- fastExponent.cpp
+++ fastExponent.cpp
@@ -4,14 +4,13 @@ using namespace std;
 
 int fastExponent(int num1, int num2) {
     int res = 1;
-    // int m = 100000;
 
     while(num2 > 0) {
         if(num2 % 2 == 1) 
             res = res * num1;
 
         num1 = num1 * num1;
-        num2 = num2 / 2;
+        num2 = num2>>1;
     }
 
     return res;
@@ -19,7 +18,7 @@ int fastExponent(int num1, int num2) {
 
 int main() {
     int num1 = 2;
-    int num2 = 20;
+    int num2 = 30;
 
     cout<<"Result of "<<num1<<" ki power "<<num2<<" is "<<fastExponent(num1, num2);
 
